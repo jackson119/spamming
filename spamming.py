6f1ac35b3b47644f07239e3c4e840cd2049b04b4
Goh Kang Sheng <kangsheng0721@gmail.com>
2020-12-17 17:20:30 +0800

    add some changes for the spamming

--- spamming.py
+++ spamming.py
@@ -5,11 +5,16 @@ Created on Tue Dec 15 21:36:16 2020
 @author: user
 """
 
-import pyautogui,time
-time.sleep(5)
-f=open("bee.txt","r")
 
-for word in f:
-    pyautogui.typewrite(word)
+import pyautogui,time
+import random
+## before started let it sleep for a while.
+time.sleep(5)
+##f=open("bee.txt","r")
+file=open("spamming.txt","r").read()
+file=file.splitlines()
+
+for _ in range(20):
+    pyautogui.typewrite(random.choice(file))
     pyautogui.press("enter")
     
